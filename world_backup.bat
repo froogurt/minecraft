26d8977476f6c70baae0453cef4e1969889075dd
froogurt <106455822+froogurt@users.noreply.github.com>
2023-11-16 22:50:32 -0500

    Backup on Thu 11/16/2023 22:50:32.36

--- world_backup.bat
+++ world_backup.bat
@@ -1,5 +1,5 @@
 cd C:\Users\kaedy\AppData\Roaming\PrismLauncher\instances\1.20.2\.minecraft\saves
 git add .
-git commit -m "Automated backup on %date% %time%"
+git commit -m "Backup on %date% %time%"
 git push origin main
 
